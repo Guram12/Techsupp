464aadd879901379453dcaef3be0e454761f5bbf
Guram12 <guram.shanidze.33@gmail.com>
2024-03-29 01:38:19 +0400

    start making Members page

--- src/components/pages/Members.jsx
+++ src/components/pages/Members.jsx
@@ -1,13 +1,33 @@
 import MembersAnimation from "../animations/MembersAnimation"
-
-
-
+import computer from "../../assets/computer_processor_1.webp"
+import matrix_background from '../../assets/matrix.webm'
+import '../styles/Members.css'
+import { Route, Routes, Link } from "react-router-dom";
 
 
 export default function Members() {
-return (
-  <div>
-    <MembersAnimation/>
-  </div>
-)
+  return (
+    <div>
+      <div className="video-background">
+        {/* <MembersAnimation/> */}
+        <video
+          src={matrix_background}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="fullscreen-video"
+        ></video>
+      </div>
+      <div>
+        <Link to="/">
+          <button className="ui-btn"   >
+            <span>
+              უკან
+            </span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
 }
