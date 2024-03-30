67ccbd03966b071bdea752df5d846663226f4dec
Guram12 <guram.shanidze.33@gmail.com>
2024-03-31 00:04:03 +0400

    scrollbar is styled

---
+++ src/components/animations/Website_cardAnimation.jsx
@@ -0,0 +1,9 @@
+import "../styles/Website_cardAnimation.css"
+
+export default function WebsiteCardAnimation() {
+  return (
+    <div className="card_website_web">
+
+    </div>
+  )
+}
--- src/components/pages/WebsitePage.jsx
+++ src/components/pages/WebsitePage.jsx
@@ -1,10 +1,13 @@
-
+import WebsiteCardAnimation from "../animations/Website_cardAnimation"
 
 
 export default function WebsitePage(){
   return (
     <div>
       WebService page
+
+      <WebsiteCardAnimation/>
+      
     </div>
   )
 }
