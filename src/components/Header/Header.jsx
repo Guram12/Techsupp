bc1e2da9c42a848e506e748fba25e6a107ea6e0b
nino <nino@ninos-MacBook-Air.local>
2024-03-30 13:53:40 +0400

    header menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,31 @@
-import React from "react";
-import { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import "../styles/Header.css";
-import logo_black from "../../assets/logo_techsupp_black.svg"
-import logo_white from "../../assets/logo_techsupp_white.svg"
+import logo_black from "../../assets/logo_techsupp_black.svg";
 import { Link } from "react-router-dom";
-
-
+import RightArrow from "../../assets/right.png";
 
 export default function Header({ isSoundOff, toggleSound }) {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header>
-      <div className="logo_and_name" >
-        <Link to="/" >
+      <div className="logo_and_name">
+        <Link to="/">
           <img src={logo_black} alt="company logo" style={{ width: "100px" }} />
         </Link>
         <h1>TechSupp</h1>
       </div>
 
       <div className="header_right">
-
-        <div className="sound_container" onClick={toggleSound}  >
+        <div className="sound_container" onClick={toggleSound}>
           {/* loader container */}
-          <div className={`typing-indicator ${isSoundOff ? "" : "stop-animation"}`}>
+          <div
+            className={`typing-indicator ${isSoundOff ? "" : "stop-animation"}`}
+          >
             <div className="typing-circle"></div>
             <div className="typing-circle"></div>
             <div className="typing-circle"></div>
@@ -35,9 +37,71 @@ export default function Header({ isSoundOff, toggleSound }) {
         <div className="dark_mode">D</div>
         <div className="menu_button">
           <div className="menu_texts">
-            <span className="menu_texts_menu">MENU</span>
-            <span className="menu_texts_close">CLOSE</span>
+            <span
+              className={`menu_texts_menu ${isOpen ? "rotate" : ""}`}
+              onClick={toggleMenu}
+            >
+              {isOpen ? "CLOSE" : "MENU"}
+            </span>
           </div>
+          {/* Render your menu links here */}
+          {isOpen && (
+            <div className="menu">
+              <div>
+                <Link className="menu_links" to="/link1">
+                  სერვისები
+                  <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link>
+                {/* <Link className="sec_menu_links" to="/link1">
+                  სერვისები                   <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link> */}
+              </div>
+              <div>
+                <Link className="menu_links" to="/link2">
+                  ჩვენს შესახებ{" "}
+                  <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link>
+                {/* <Link className="sec_menu_links" to="/link2">
+                  ჩვენს შესახებ                   <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link> */}
+              </div>
+
+              <div>
+                <Link className="menu_links" to="/link3">
+                  წევრები
+                  <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link>
+                {/* <Link className="sec_menu_links" to="/link3">
+                  წევრები                   <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link> */}
+              </div>
+              <div>
+                <Link className="menu_links" to="/link3">
+                  კონტაქტები{" "}
+                  <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link>
+                {/* <Link className="sec_menu_links" to="/link3">
+                  კონტაქტები                   <span>
+                    <img src={RightArrow} alt="" />
+                  </span>
+                </Link> */}
+              </div>
+            </div>
+          )}
           <div className="menu_dots">
             <div></div>
             <div></div>
@@ -46,5 +110,4 @@ export default function Header({ isSoundOff, toggleSound }) {
       </div>
     </header>
   );
-};
-
+}
