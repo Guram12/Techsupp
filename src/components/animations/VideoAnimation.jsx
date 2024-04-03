4bfbf424cd7d16f638c2671d53d6ba2ba2dff346
Guram12 <guram.shanidze.33@gmail.com>
2024-04-03 20:02:45 +0400

    scroll is disabled when animation video is opened

--- src/components/animations/VideoAnimation.jsx
+++ src/components/animations/VideoAnimation.jsx
@@ -12,21 +12,20 @@ export default function VideoAnimation() {
   const [isChecked3, setIsChecked3] = useState(false);
 
 
-  const handleCheckboxChange1 = (event) => {
-    event.stopPropagation(); // Prevent click from propagating
-    setIsChecked1(!isChecked1);
-    // if (!isChecked1) setIsChecked2(false); 
-  };
+  // const handleCheckboxChange1 = (event) => {
+  //   event.stopPropagation(); 
+  //   setIsChecked1(!isChecked1);
+  // };
 
-  const handleCheckboxChange2 = (event) => {
-    event.stopPropagation();
-    setIsChecked2(!isChecked2);
-  };
+  // const handleCheckboxChange2 = (event) => {
+  //   event.stopPropagation();
+  //   setIsChecked2(!isChecked2);
+  // };
 
-  const handleCheckboxChange3 = (event) => {
-    event.stopPropagation(); 
-    setIsChecked3(!isChecked3);
-  };
+  // const handleCheckboxChange3 = (event) => {
+  //   event.stopPropagation(); 
+  //   setIsChecked3(!isChecked3);
+  // };
 
 
   useEffect(() => {
@@ -34,23 +33,43 @@ export default function VideoAnimation() {
     const closeVideos = () => {
       setIsChecked1(false);
       setIsChecked2(false);
-      setIsChecked3(false)
+      setIsChecked3(false);
+      document.querySelector('body').style.overflow = 'auto'; // Enable scroll when closing videos
     };
-    const maindiv = document.querySelector(".main_animation_page_container")
+    const maindiv = document.querySelector(".main_animation_page_container");
     maindiv.addEventListener('click', closeVideos);
 
-    // Clean up the global click listener
     return () => {
       maindiv.removeEventListener('click', closeVideos);
     };
   }, []);
 
+  const handleCheckboxChange = (checkedSetter) => (event) => {
+    event.stopPropagation(); // Prevent click from propagating
+    const newValue = event.target.checked; // Use the checked value directly
+    console.log(newValue);
+    checkedSetter(newValue); // Update the state with the new value
+    // After state update, check if any checkbox is checked to determine scroll behavior
+    toggleScroll();
+  };
+  
+
+  // Function to toggle the website scroll
+  const toggleScroll = () => {
+    const body = document.querySelector('body');
+    if (body.style.overflow !== 'hidden') {
+      body.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = 'auto';
+    }
+  };
 
   return (
     <div className='main_animation_page_container' onClick={(e) => e.stopPropagation()}   >
+      {/* 1 video animation  */}
       {!isChecked2 && !isChecked3 && (
         <div className="wrapper">
-          <input type="checkbox" checked={isChecked1} onChange={handleCheckboxChange1} />
+          <input type="checkbox" checked={isChecked1} onChange={handleCheckboxChange(setIsChecked1)} />
           <div className="video">
             <video src={night_basement} loop muted autoPlay playsInline></video>
           </div>
@@ -59,9 +78,10 @@ export default function VideoAnimation() {
           </div>
         </div>
       )}
+      {/* 2 video animation  */}
       {!isChecked1 && !isChecked3 && (
         <div className="wrapper1">
-          <input type="checkbox" checked={isChecked2} onChange={handleCheckboxChange2} />
+          <input type="checkbox" checked={isChecked2} onChange={handleCheckboxChange(setIsChecked2)} />
           <div className="video">
             <video src={splashvideo} loop muted autoPlay playsInline></video>
           </div>
@@ -70,9 +90,10 @@ export default function VideoAnimation() {
           </div>
         </div>
       )}
+      {/* 3 video animation  */}
       {!isChecked2 && !isChecked1 && (
         <div className="wrapper2">
-          <input type="checkbox" checked={isChecked3} onChange={handleCheckboxChange3} />
+          <input type="checkbox" checked={isChecked3}  onChange={handleCheckboxChange(setIsChecked3)} />
           <div className="video">
             <video src={techsupp_name_video} loop muted autoPlay playsInline></video>
           </div>
