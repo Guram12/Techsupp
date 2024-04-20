9c0dcd3e2c5136f86e8bd8beb20d8223532dba83
Guram12 <guram.shanidze.33@gmail.com>
2024-04-20 16:33:31 +0400

    amaqvs rom im idiotma chamopulos

---
+++ src/components/pages/SocialMedia.jsx
@@ -0,0 +1,22 @@
+import "../styles/SocialMedia.css"
+import transition from "../Header/Transition"
+
+
+
+function SocialMedia() {
+  return (
+    <div style={{paddingTop: "100px"}} >
+      social media page 
+    </div>
+  )
+}
+
+export default transition(SocialMedia);
+
+
+
+
+// 3 პაკეტი , ფ, ინსტაგ, ლინკდინი, ტწიტერ 
+
+
+
