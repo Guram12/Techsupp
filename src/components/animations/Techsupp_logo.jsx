fbc22b405a5cd6448b2d2bba2b45794f11701772
Guram12 <guram.shanidze.33@gmail.com>
2024-04-21 22:16:10 +0400

    contact styles are readd for desktop

--- src/components/animations/Techsupp_logo.jsx
+++ src/components/animations/Techsupp_logo.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import "../styles/Techsupp_logo.css";
 
-const AnimatedLogo = () => {
+function AnimatedLogo({ isDarkmodeOn }) {
 
     return (
-        <svg style={{width: "500px", height: "500px"}} className="logo" data-bbox="0.03 9.97 389.62 238.56" viewBox="0 0 389.67 254.4" xmlns="http://www.w3.org/2000/svg" data-type="ugc">
+        <svg style={{ width: "500px", height: "500px" }} className={ isDarkmodeOn ?  "big_logo"  :  "big_logo_dark"} data-bbox="0.03 9.97 389.62 238.56" viewBox="0 0 389.67 254.4" xmlns="http://www.w3.org/2000/svg" data-type="ugc">
             <g>
-                <path className="path5" d="m.03 10.38 144-.37" fill="none" stroke="#000000" stroke-miterlimit="10" stroke-width="30"/>
-                <path className="path4" d="M42.58 70.01h59" fill="none" stroke="#000000" stroke-miterlimit="10" stroke-width="30" />
-                <path className="path1"  d="m164.47 248.53 85.93-118.56h62.18" fill="none" stroke="#000000" stroke-miterlimit="10" stroke-width="30" />
-                <path className="path2" d="M125.21 194.83 222.99 65.2l130.37.13" fill="none" stroke="#000000" stroke-miterlimit="10" stroke-width="30" />
-                <path className="path3" d="M91.14 146.68q52.4-68.34 104.81-136.71l193.7.37" fill="none" stroke="#000000" stroke-miterlimit="10" stroke-width="30" />
+                <path className="path5" d="m.03 10.38 144-.37" fill="none" stroke={ isDarkmodeOn ?  "#000000" : "white"} stroke-miterlimit="10" stroke-width="30" />
+                <path className="path4" d="M42.58 70.01h59" fill="none" stroke={ isDarkmodeOn ?  "#000000" : "white"} stroke-miterlimit="10" stroke-width="30" />
+                <path className="path1" d="m164.47 248.53 85.93-118.56h62.18" fill="none" stroke={ isDarkmodeOn ?  "#000000" : "white"} stroke-miterlimit="10" stroke-width="30" />
+                <path className="path2" d="M125.21 194.83 222.99 65.2l130.37.13" fill="none" stroke={ isDarkmodeOn ?  "#000000" : "white"} stroke-miterlimit="10" stroke-width="30" />
+                <path className="path3" d="M91.14 146.68q52.4-68.34 104.81-136.71l193.7.37" fill="none" stroke={ isDarkmodeOn ?  "#000000" : "white"} stroke-miterlimit="10" stroke-width="30" />
             </g>
         </svg>
     );
